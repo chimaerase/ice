f43e4400f5792ea4c66db280df07315d10faa94f
Sarah LaFrance <SLFrance@slfrance-mr.dhcp.lbl.gov>
2015-09-10 17:04:52 -0700

    did some work on adding frontend myseq capability, using filename filter atm

--- src/main/webapp/scripts/entry/entryController.js
+++ src/main/webapp/scripts/entry/entryController.js
@@ -137,6 +137,7 @@ angular.module('ice.entry.controller', [])
             $scope.traceSequences = result;
         });
 
+
         $scope.traceSequenceUploader = new FileUploader({
             scope: $scope, // to automatically update the html. Default: $rootScope
             url: "rest/parts/" + entryId + "/traces",
